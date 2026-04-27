cbca5692036a2ea236bd8833f74a5646b3be54fa
Kolin-ko <kkolinn4457@gmail.com>
2026-04-27 18:37:37 +0700

    Cepuaa

---
+++ cepu_app/lib/services/post_service.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:cepu_app/models/post.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class PostService {
+  static final FirebaseFirestore _database = FirebaseFirestore.instance;
+  static final CollectionReference _postCollection = _database.collection('post');
+  // static final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<void> addPost(Post post) async {
+    Map<String, dynamic> newNote = {
+    'image': post.image,
+    'description': post.description,
+    'category' : post.category,
+    'latitude': post.latitude,
+    'longitude': post.longitude,
+    'user_Id': post.userId,
+    'fullName': post.fullName,
+    // 'image_url': post. imageUrl,
+    'created_at': FieldValue.serverTimestamp(),
+    'updated_at': FieldValue. serverTimestamp(),
+    };
+    await _postCollection.add(newNote);
+}
+
+  static Future<void> updatePost (Post post) async {
+    Map<String, dynamic> updatedNote = {
+      'image': post.image,
+      'description': post.description,
+      'latitude': post.latitude,
+      'longitude': post.longitude,
+      'created_at': post.createdAt,
+      'user_Id': post.userId,
+      'fullName': post.fullName,
+      'updated_at': FieldValue.serverTimestamp(),
+    };
+    await _postCollection.doc(post.id).update(updatedNote);
+  }
+
+  static Stream<List<Post>> getPostList() {
+    return _postCollection.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return Post(
+          id: doc.id,
+          image: data['image'],
+          description: data['description'],
+          category: data['category'],
+          createdAt: data['created_at'] != null
+            ? data['created_at'] as Timestamp
+            : null,
+          updatedAt: data ['updated_at' ] != null
+            ? data ['updated_at'] as Timestamp
+            : null,
+          latitude: data['latitude'],
+          longitude: data['longitude'],
+          userId: data['user_Id'],
+          fullName: data['fullName']
+          );
+        }).toList();
+    });
+  }
+
+  static Future<void> deletePost (Post post) async {
+    await _postCollection.doc(post.id).delete();
+  }
+
+  static Future<QuerySnapshot> retrievePost() async {
+    return await _postCollection.get();
+  }
+  
+}
